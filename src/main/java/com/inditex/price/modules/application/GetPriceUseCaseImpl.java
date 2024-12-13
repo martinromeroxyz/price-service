873b5cfa53d407e441b98b366d1dd5e77e15cf0a
martinromeroxyz <martinromeroxyz@gmail.com>
2024-12-13 13:55:55 +0100

    feature/command object

--- src/main/java/com/inditex/price/modules/application/GetPriceUseCaseImpl.java
+++ src/main/java/com/inditex/price/modules/application/GetPriceUseCaseImpl.java
@@ -1,12 +1,12 @@
 package com.inditex.price.modules.application;
 
 import com.inditex.price.model.PriceDTO;
+import com.inditex.price.modules.domain.filter.PriceFilter;
 import com.inditex.price.modules.domain.model.Price;
 import com.inditex.price.modules.domain.repository.PriceRepository;
 import com.inditex.price.modules.domain.usecase.GetPriceUseCase;
 import com.inditex.price.modules.infrastructure.error.PriceException;
 import com.inditex.price.modules.infrastructure.persistence.converter.PriceToPriceDTOConverter;
-import com.inditex.price.modules.domain.filter.PriceFilter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
--- src/main/java/com/inditex/price/modules/infrastructure/web/PriceController.java
+++ src/main/java/com/inditex/price/modules/infrastructure/web/PriceController.java
@@ -1,24 +1,24 @@
-package com.inditex.price.modules.infrastructure.web.controller;
+package com.inditex.price.modules.infrastructure.web;
 
 import com.inditex.price.api.PriceApi;
 import com.inditex.price.model.PriceDTO;
-import com.inditex.price.modules.domain.filter.PriceFilter;
+import com.inditex.price.model.PriceFilterDTO;
 import com.inditex.price.modules.domain.usecase.GetPriceUseCase;
+import com.inditex.price.modules.infrastructure.persistence.converter.PriceFilterDTOToPriceFilterConverter;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
 
 @RestController
 @AllArgsConstructor
 public class PriceController implements PriceApi {
 
-    private GetPriceUseCase getPriceUseCase;
+    private final GetPriceUseCase getPriceUseCase;
+    private final PriceFilterDTOToPriceFilterConverter priceFilterDTOToPriceFilterConverter;
 
     @Override
-    public ResponseEntity<PriceDTO> _getPrice(String productId, String brandId, LocalDateTime date) {
+    public ResponseEntity<PriceDTO> _getPrice(PriceFilterDTO filter) {
 
-        return ResponseEntity.ok(getPriceUseCase.getPrice(PriceFilter.builder().productId(productId).brandId(brandId).date(date).build()));
+        return ResponseEntity.ok(getPriceUseCase.getPrice(priceFilterDTOToPriceFilterConverter.convert(filter)));
     }
 }
